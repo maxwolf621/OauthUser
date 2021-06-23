c8672097bfc998f5e139fee808369b0651225f52
Maxwolf621 <jervismayer@gmail.com>
2021-06-23 20:10:27 +0800

    3thPartyLogin

---
+++ demo/src/main/java/com/githublogin/demo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.githublogin.demo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
